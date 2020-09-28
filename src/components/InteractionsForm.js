5d024f4f95125a8b4d1d4a608df6165e53942085
github username <constance.hyman@mchsi.com>
2020-09-27 22:50:55 -0700

    Add Interactions Form

---
+++ src/components/InteractionsForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class InteractionsForm extends React.Component {
+  render() {
+    return(
+      <div>
+        InteractionsForm
+      </div>
+    );
+  }
+}
+
+
+export default InteractionsForm;
