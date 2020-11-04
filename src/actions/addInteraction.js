9680f9b7c574b54481eda93d4525c701b231cf8e
github username <constance.hyman@mchsi.com>
2020-11-04 04:21:55 -0800

    Build addInteraction action creator

---
+++ src/actions/addInteraction.js
@@ -0,0 +1,21 @@
+export const addInteraction = (interaction, contactId) => {
+
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/contacts/${contactId}/interactions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(interaction)
+    })
+    .then(response => response.json())
+    .then(contact => {
+        if (contact.error) {
+          alert(contact.error)
+        } else {
+          dispatch({type: 'ADD_INTERACTION', payload: contact})
+        }
+      }
+    )
+  }
+}
