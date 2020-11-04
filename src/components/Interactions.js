20dbd0885eb4157801d6c171c7881c0a88a6d5dc
github username <constance.hyman@mchsi.com>
2020-11-04 04:16:34 -0800

    Import connect and deleteInteraction, build out Interactions component

--- src/components/Interactions.js
+++ src/components/Interactions.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {deleteInteraction} from '../actions/deleteInteraction';
 
 const Interactions = (props) => {
+
+const handleDelete = (interaction) => {
+  props.deleteInteraction(interaction.id, interaction.contact_id)
+}
+
   return(
     <div>
-      Interactions
+      {props.interactions && props.interactions.map(interaction =>
+        <li key={interaction.id}>{interaction.date}<br/> {interaction.description}<br/> {interaction.notes} <button onClick={() => handleDelete(interaction)}>Delete</button><br/></li>
+      )}
     </div>
   );
 }
 
 
-export default Interactions;
+export default connect(null, {deleteInteraction})(Interactions);
