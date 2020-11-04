9675327878b29773458dae1c63dbfcf5104ed403
github username <constance.hyman@mchsi.com>
2020-11-04 04:19:13 -0800

    Add actions to reducer

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,12 +1,29 @@
 
 
 export default function contactReducer(state = {contacts: []}, action) {
-
   switch (action.type) {
     case 'FETCH_CONTACTS':
       return {contacts: action.payload}
     case 'ADD_CONTACT':
       return {...state, contacts: [...state.contacts, action.payload]}
+    case 'ADD_INTERACTION':
+      let contacts = state.contacts.map(contact => {
+        if (contact.id === action.payload.id) {
+          return action.payload
+        }else{
+          return contact
+        }
+      })
+      return {...state, contacts: contacts}
+    case 'DELETE_INTERACTION':
+      let contacts2 = state.contacts.map(contact => {
+        if (contact.id === action.payload.id) {
+          return action.payload
+        }else{
+          return contact
+        }
+      })
+      return {...state, contacts: contacts2}
     default:
       return state
   }
